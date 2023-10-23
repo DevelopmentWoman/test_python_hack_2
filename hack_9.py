fb48f1fb401945b5529e7ea83d4f746905166882
Yelitza Suniaga <87539202+DevelopmentWoman@users.noreply.github.com>
2023-10-23 07:16:33 -0500

    Update hack_9.py

--- hack_9.py
+++ hack_9.py
@@ -4,10 +4,9 @@ text: {"foo":"fookziman","bar":"barziman"} output => {"Foo":"Fooziman"}
 
 
 def fn_hack_9(result):
-    result.pop("bar",None)
     result ={k.capitalize() if k == "foo" else k: (v.capitalize()).replace("ook","oo") for k,v in result.items()}
+    result.pop("bar")
     return result
 
 
 
-# print(fn_hack_9({"foo":"fookziman","bar":"barziman"}))
